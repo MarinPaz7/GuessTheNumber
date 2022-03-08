081cb05d8d12883283fbca0187628756ae08b156
MarinPaz7 <99123733+MarinPaz7@users.noreply.github.com>
2022-03-08 22:58:57 +0100

    Initial commit

---
+++ PogodiBroj.py
@@ -0,0 +1,8 @@
+secret = 97
+
+guess = int(input("Guess the secret number (between 1 and 100): "))
+
+if guess == secret:
+    print("Congratulations! You have guessed, it's number 97. Are you a wizard?")
+else:
+    print("Too bad, unfortunately that was unlucky guess. Better luck next time ;) ")
